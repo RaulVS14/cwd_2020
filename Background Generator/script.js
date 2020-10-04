14c5dafc0bec29caf0cc33e00307e8c79565b2be
raulm <raul.muser@gmail.com>
2020-10-05 00:17:45 +0300

    Add TODO for the BG Generator

--- Background Generator/script.js
+++ Background Generator/script.js
@@ -11,3 +11,10 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+/*
+* TODO:
+*  1. Write code so that the colour inputs match the background generated on the first page load.
+*  2. Display the initial CSS linear gradient property on page load.
+*  3. BONUS: Add a random button which generates two random numbers for the colour inputs.
+* */

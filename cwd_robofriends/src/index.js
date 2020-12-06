850954cca49a605505e5c10ab07c72d5d7ac1f7c
raulm <raul.muser@gmail.com>
2020-12-06 21:12:29 +0200

    Add search bar and title for the page and refactor them into App.js

--- cwd_robofriends/src/index.js
+++ cwd_robofriends/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {robots} from "./robots";
 
 ReactDOM.render(
     <React.StrictMode>
-        <CardList robots={robots}/>
+        <App/>
     </React.StrictMode>,
     document.getElementById('root')
 );

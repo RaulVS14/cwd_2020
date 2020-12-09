3b6ac8b44909198a124e3defeb0d33fe7dda9075
raulm <raul.muser@gmail.com>
2020-12-09 20:28:28 +0200

    Remove error from CardList

--- cwd_robofriends/src/components/CardList.js
+++ cwd_robofriends/src/components/CardList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from "./Card";
 
 const CardList = ({robots}) => {
-    throw new Error;
     return (
         <div>
             {robots.map((el) => <Card key={el.id} id={el.id} name={el.name} email={el.email}/>)}
